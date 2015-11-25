30eec8d1f58df8679056dfd53784c0b383e93866
HustMaroon <thanhbk6294@gmail.com>
2015-11-25 18:23:04 +0700

    1st commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  		redirect_to user_url(user)
+  		flash[:success] = 'Log in successfully'
+  	else
+  		flash.now[:danger] = 'Invalid email/pass combination'
+  		render 'new'
+  	end
+  end
+  
+  def destroy
+    log_out(@current_user)
+    redirect_to static_pages_home_path
+  end
+end
