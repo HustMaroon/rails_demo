97487ceccf062736f68da5533aadd7d6d1e5826e
HustMaroon <thanhbk6294@gmail.com>
2015-12-23 17:39:52 +0700

    add user follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,16 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, presence: true, length: {minimum: 1}, allow_nil: true
 
+	has_many :active_relationships, class_name: "Relationship",
+	                                foreign_key: "follower_id",
+	                                dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
+
+	has_many :passive_relationships, class_name: "Relationship",
+	                                 foreign_key: "followed_id"
+	has_many :followers, through: :passive_relationships, source: :follower
+
+
 	def downcase_email
 		self.email = email.downcase
 	end
@@ -67,6 +77,19 @@ class User < ActiveRecord::Base
 	end
 
 	def feed
-		Post.where("user_id = ?", id)
+		following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+		Post.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+	end
+
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def following?(other_user)
+		following.include?(other_user)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,14 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
 
   resources :posts, only: [:create, :destroy]
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
